3c2a6cb941b84089863435bf2ea1e5640d0d4b5e
Pham Doan Kiem <92410976+m3iK18dp@users.noreply.github.com>
2023-04-06 23:22:38 +0700

    06/04 11:22

---
+++ mucsicmanage-reactjs/src/main.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./css/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./page/App";
+import Login from "./page/Login";
+import Register from "./page/Register";
+import Songs from "./page/Songs";
+import MyAccount from "./page/MyAccount";
+import Users from "./page/Users";
+import ErrorPage from "./page/ErrorPage";
+import UploadUser from "./page/UploadUser";
+import UploadSong from "./page/UploadSong";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/songs" element={<Songs />} />
+      <Route
+        path="/songs/:id"
+        element={
+          !localStorage.getItem("token") ? (
+            <ErrorPage code={403} />
+          ) : (
+            <UploadSong />
+          )
+        }
+      />
+      <Route
+        path="/users"
+        element={
+          localStorage.getItem("roles") &&
+          localStorage.getItem("roles").includes("ROLE_ADMIN") ? (
+            <Users />
+          ) : (
+            <ErrorPage code={403} />
+          )
+        }
+      />
+      <Route
+        path="/users/:id"
+        element={
+          localStorage.getItem("roles") &&
+          localStorage.getItem("roles").includes("ROLE_ADMIN") ? (
+            <UploadUser />
+          ) : (
+            <ErrorPage code={403} />
+          )
+        }
+      />
+      <Route
+        path="/account/:username"
+        element={
+          !localStorage.getItem("token") ? (
+            <ErrorPage code={403} />
+          ) : (
+            <MyAccount />
+          )
+        }
+      />
+      <Route path="*" element={<ErrorPage code={404} />} />
+      <Route path="/error/:code" element={<ErrorPage />} />
+    </Routes>
+  </BrowserRouter>
+);
--- mucsicmanage-reactjs/src/services/Request.jsx
+++ mucsicmanage-reactjs/src/services/Request.jsx
@@ -1,63 +1,63 @@
-import axios from 'axios';
-import queryString from 'query-string';
-const API_BASE_URL = 'http://localhost:8080/api';
+import axios from "axios";
+import queryString from "query-string";
+const API_BASE_URL = "http://localhost:8080/api";
 
 const callApi = async (
-	navigate,
-	endpoint,
-	method = 'get',
-	data = null,
-	params = {},
-	headers = {},
+  navigate,
+  endpoint,
+  method = "get",
+  data = null,
+  params = {},
+  headers = {}
 ) => {
-	const options = {
-		url: `${API_BASE_URL}/${endpoint}`,
-		method,
-		headers,
-		timeout: 5000,
-	};
-	if (
-		!(
-			endpoint.includes('auth') ||
-			(endpoint.includes('songs') && method === 'get')
-		)
-	) {
-		options.headers = {
-			...headers,
-			Authorization: 'Bearer ' + localStorage.getItem('token'),
-		};
-	}
-	if (method.toLowerCase() === 'get') {
-		if (Object.keys(params).length !== 0)
-			options.url = options.url + '?' + queryString.stringify(params);
-	} else {
-		options.data = data;
-	}
-	try {
-		return await axios(options).then((res) => res.data);
-	} catch (error) {
-		if (error.code === 'ECONNABORTED') {
-			// Lỗi timeout
-			navigate('/error/408');
-		} else if (error.response) {
-			// Lỗi xử lý response
-			const { status, data } = error.response;
-			if (status === 404) {
-				navigate('/error/404');
-			} else if (status === 401) {
-				navigate('/error/401');
-			} else {
-				console.log(
-					`Server responded with error code ${status} and message: ${data}`,
-				);
-			}
-		} else if (error.request) {
-			// Lỗi kết nối
-			navigate('/error/500');
-		} else {
-			// Lỗi xử lý request
-			console.log('Request processing error:', error);
-		}
-	}
+  const options = {
+    url: `${API_BASE_URL}/${endpoint}`,
+    method,
+    headers,
+    timeout: 5000,
+  };
+  if (
+    !(
+      endpoint.includes("auth") ||
+      (endpoint.includes("songs") && method === "get")
+    )
+  ) {
+    options.headers = {
+      ...headers,
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    };
+  }
+  if (method.toLowerCase() === "get") {
+    if (Object.keys(params).length !== 0)
+      options.url = options.url + "?" + queryString.stringify(params);
+  } else {
+    options.data = data;
+  }
+  try {
+    return await axios(options).then((res) => res.data);
+  } catch (error) {
+    if (error.code === "ECONNABORTED") {
+      // Lỗi timeout
+      navigate("/error/408");
+    } else if (error.response) {
+      // Lỗi xử lý response
+      const { status, data } = error.response;
+      if (status === 404) {
+        navigate("/error/404");
+      } else if (status === 401) {
+        navigate("/error/401");
+      } else {
+        console.log(
+          `Server responded with error code ${status} and message: ${data}`
+        );
+      }
+    } else if (error.request) {
+      // Lỗi kết nối
+      navigate("/error/500");
+    } else {
+      // Lỗi xử lý request
+      console.log("Request processing error:", error);
+    }
+  }
 };
 export default callApi;
